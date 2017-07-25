eece1e6f158fe9a96b43f0d14fcb5f7a7d9c1506
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-07-25 14:27:01 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    array.select {|word| word.split("").sort == @name.split("").sort}
+  end
+  # splits on each letter!! word.split. sort (will account for dif arrangement of lettters
+  # split on each letter of the name )
+
+#  takes an array of possible anagrams ...returns all matches in the array
+#  if no matches exist in the array >> return empty array
+end
